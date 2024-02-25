0dd417086ef34093bc6a05009bbb4bd8868c858f
jeremy lee <jerslee@gmail.com>
2024-02-25 17:30:49 -0600

    Pylint Score 9.65/10

--- set_creds.py
+++ set_creds.py
@@ -1,8 +1,24 @@
+"""
+Module: login_interface.py
+Description: GUI for device login info
+"""
+
 import customtkinter as ctk
 from CTkMessagebox import CTkMessagebox
 
 class LoginUI:
+    """
+    Class: LoginUI
+    Description: Represents the login interface.
+    """
+
     def __init__(self, master):
+        """
+        Method: __init__
+        Description: Initializes the LoginUI class.
+        Parameters:
+            - master: The master widget.
+        """
         self.master = master
         self.master.title("Set Credentials")
 
@@ -18,13 +34,25 @@ class LoginUI:
 
         self.ip_entry_frames = []
 
-        self.button_add_ip = ctk.CTkButton(master, fg_color="transparent", border_width=2, text="Add IP Address", command=self.add_ip_entry_frame)
+        self.button_add_ip = ctk.CTkButton(master,
+                                           fg_color="transparent",
+                                           border_width=2,
+                                           text="Add IP Address",
+                                           command=self.add_ip_entry_frame)
         self.button_add_ip.grid(row=2, column=1, pady=10, padx=5, sticky="news")
 
-        self.button_login = ctk.CTkButton(master, fg_color="transparent", border_width=2, text="Update", command=self.login)
+        self.button_login = ctk.CTkButton(master,
+                                          fg_color="transparent",
+                                          border_width=2,
+                                          text="Update",
+                                          command=self.login)
         self.button_login.grid(row=3, column=1, pady=10, padx=5, sticky="news")
 
     def add_ip_entry_frame(self):
+        """
+        Method: add_ip_entry_frame
+        Description: Adds entry fields for IP addresses dynamically.
+        """
         ip_entry_frame = ctk.CTkFrame(self.master)
         ip_entry_frame.grid(sticky="ew", padx=10, pady=5)
         ip_entry = ctk.CTkEntry(ip_entry_frame)
@@ -34,9 +62,17 @@ class LoginUI:
         self.button_login.grid_forget()  # Temporarily forget the "Login" button
         for i, frame in enumerate(self.ip_entry_frames, start=3):
             frame.grid(row=i, column=1, pady=5, sticky="ew")
-        self.button_login.grid(row=len(self.ip_entry_frames) + 3, column=1, pady=10, sticky="se")  # Re-add the "Login" button
+        self.button_login.grid(row=len(self.ip_entry_frames) + 3,
+                               column=1, pady=10,
+                               sticky="se")  # Re-add the "Login" button
 
     def get_ip_addresses(self):
+        """
+        Method: get_ip_addresses
+        Description: Retrieves the IP addresses entered by the user.
+        Returns:
+            - ip_addresses: A list of IP addresses.
+        """
         ip_addresses = []
         for frame in self.ip_entry_frames:
             ip_entry = frame.grid_slaves(row=0, column=0)[0]
@@ -44,26 +80,39 @@ class LoginUI:
         return ip_addresses
 
     def login(self):
+        """
+        Method: login
+        Description: Writes login variables to "credentials.py".
+        """
         username = self.entry_username.get()
         password = self.entry_password.get()
         ip_addresses = self.get_ip_addresses()
 
         try:
-            with open("credentials.py", "w") as file:
+            with open("credentials.py", "w", encoding="utf-8") as file:
                 file.write(f'\n# Credentials for {username}\n')
                 file.write(f'USERNAME = "{username}"\n')
                 file.write(f'PASSWORD = "{password}"\n')
                 for i, ip in enumerate(ip_addresses, start=1):
                     file.write(f'IP_ADDRESS_{i} = "{ip}"\n')
-            CTkMessagebox(master=self.master, title="Success!", message="Credentials have been updated!", icon="check")
+            CTkMessagebox(master=self.master,
+                          title="Success!",
+                          message="Credentials have been updated!",
+                          icon="check")
         except Exception as e:
-            CTkMessagebox(title="Error!", message=f"Failed to update credentials file: {e}", icon="error")
+            CTkMessagebox(title="Error!",
+                          message=f"Failed to update credentials file: {e}",
+                          icon="error")
 
 def main():
+    """
+    Function: main
+    Description: Sets up the Tkinter application window.
+    """
     ctk.set_appearance_mode("dark")
     ctk.set_default_color_theme("dark-blue")
     root = ctk.CTk()
-    app = LoginUI(root)
+    LoginUI(root)
     root.mainloop()
 
 if __name__ == "__main__":
